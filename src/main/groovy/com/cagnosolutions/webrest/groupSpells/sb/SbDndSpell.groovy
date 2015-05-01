32f3a33896f331a1eae0b5b014391536d606f515
gregpechiro <gregpechiro@yahoo.com>
2015-05-01 15:08:43 -0400

    major overhaul: not quite finished

---
+++ src/main/groovy/com/cagnosolutions/webrest/groupSpells/sb/SbDndSpell.groovy
@@ -0,0 +1,28 @@
+package com.cagnosolutions.webrest.groupSpells.sb
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+@Entity
+class SbDndSpell {
+
+	@Id
+	@GeneratedValue
+	Integer id
+
+
+	String school
+
+	String subschool
+
+	String rulebook
+	ArrayList<String> descriptors
+	Integer page
+
+	@Column(columnDefinition = "LONGTEXT")
+	String descriptionHtml
+	String name, castingTime, spellRange, target, effect, area, duration, savingThrow, spellResistance
+	Boolean verbalComponent, somaticComponent, materialComponent, arcaneFocusComponent, divineFocusComponent, xpComponent, metaBreathComponent, trueNameComponent, corruptComponent
+}
